49cf29d7d77dfb69cc0d7188529a9e5f3627ce03
balasgit <bala@us.ibm.com>
2022-09-01 13:46:00 -0500

    fix compile errors

--- test/stages/stage2-lb.tf
+++ test/stages/stage2-lb.tf
@@ -1,7 +1,8 @@
 module "lb" {
   source = "./module"
 
-  resource_group_name = module.resource_group.name
+  resource_group_id   = module.resource_group.id
+  
   vpc_name            = module.subnets.vpc_name
   subnet_ids          = module.subnets.ids
   label               = "test"
