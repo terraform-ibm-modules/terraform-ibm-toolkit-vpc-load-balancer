9061cec3f1dcd5c3f9e14899f8556f8fd23124a3
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-01 13:56:29 -0500

    update README/CN - Signed-off-by: Bala <@balasgit> (#4)
    * update README/CN - Signed-off-by: Bala <@balasgit>
    
    * fix compile errors
    
    * Fix compile error
    
    * fix compile errors
    
    * Fix compile errors

--- test/stages/stage2-lb.tf
+++ test/stages/stage2-lb.tf
@@ -2,6 +2,7 @@ module "lb" {
   source = "./module"
 
   resource_group_id   = module.resource_group.id
+  
   vpc_name            = module.subnets.vpc_name
   subnet_ids          = module.subnets.ids
   label               = "test"
